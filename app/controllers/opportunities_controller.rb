a4beae60d1195938583b1d1ce558b9107af272ca
afreem66 <afreem66@gmail.com>
2015-11-18 16:41:30 -0500

    fixed error code 300 by using city name instead of zip code

--- app/controllers/opportunities_controller.rb
+++ app/controllers/opportunities_controller.rb
@@ -5,13 +5,18 @@ class OpportunitiesController < ApplicationController
 
   def find
     @location = opportunity_params[:postCode]
-
     api = VolunteerMatchApi.new($account_name, $api_key)
-    response = api.search(@location).opportunities # JSON {"name":"VolunteerMatch","result":"Hello VolunteerMatch!"}
-    puts 'andrew'
-    puts response
-
-    redirect_to results_path
+    response = api.search(@location).opportunities[0] # JSON {"name":"VolunteerMatch","result":"Hello VolunteerMatch!"}
+      # response.each do |opp|
+      #   @opportunities.push(
+      #                         :title => opp.title,
+      #                         :parentOrg => opp.parentOrg,
+      #                         :location => opp.location,
+      #                         :description => opp.description,
+      #                         :url => opp.url
+      #                       )
+      # end
+    redirect_to results_path(response.to_s)
   end
 
   def results
@@ -35,27 +40,31 @@ class VolunteerMatchApi
 
   def search (postCode)
     call :searchOpportunities,
-    { :opportunityTypes => ["public","private"],
+    { :opportunityTypes => ["public"],
       :location => postCode,
-      :fieldsToDisplay => ["name", "url", "location", "description", "mission"]
+      :fieldsToDisplay => ["title", "parentOrg", "url", "location", "description", "mission"],
+      :nbOfResults => 10,
+      :sortCritera => "distance",
+      :sortOrder => "desc"
     }.to_json
   end
 
   def call(action, json_query)
     puts action
     puts json_query
+    # fail
     nonce           = Digest::SHA2.hexdigest(rand.to_s)[0, 20]
     creation_time   = Time.now.utc.strftime("%Y-%m-%dT%H:%M:%S%z")
     password_digest = Base64.encode64(Digest::SHA2.digest(nonce + creation_time + @api_key)).chomp
-    url             = URI.parse("http://www.volunteermatch.org/api/call?action=#{action.to_s}&query=" + URI.encode(json_query))
+    url             = URI.parse("http://www.volunteermatch.org/api/call?action=#{action.to_s}&query=#{json_query}")
 
     req             = Net::HTTP::Get.new(url.request_uri)
     req.add_field('Content-Type', 'application/json')
     req.add_field('Authorization', 'WSSE profile="UsernameToken"')
     req.add_field('X-WSSE', 'UsernameToken Username="' + @account_name + '", PasswordDigest="' + password_digest + '", ' +
         'Nonce="' + nonce + '", Created="' + creation_time + '"')
-
     res = Net::HTTP.new(url.host, url.port).start { |http| http.request(req) }
+    # fail
     raise "HTTP error code #{res.code}" unless res.code == "200"
     OpenStruct.new(JSON.parse res.body)
   end
