50109fb411da831a507a2759ed65a6ed99cfa23d
afreem66 <afreem66@gmail.com>
2015-11-17 23:06:22 -0500

    created user log in

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,2 +1,15 @@
 class SessionController < ApplicationController
+
+  def create
+    user = User.find_by(email: user_params[:email])
+
+    if user && user.authenticateIuser_params[:password]
+      session[:current_user_id] = user_id
+      redirect_to results
+    else
+      flash[:message]  = "Email / Password combo does not exist"
+      redirect_to root_path
+    end
+  end
+
 end
