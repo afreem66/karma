b2515af4d0902f17bc140d1b12b24ef8eddb2036
afreem66 <afreem66@gmail.com>
2015-11-17 22:19:53 -0500

    made create user method:

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,16 @@
 class UsersController < ApplicationController
 
 def create
+  @user = User.new(user_params)
+
+  if @user.save
+      flash[:message] =  "logged in"
+  else
+    flash[:message] = "not loged in, give it another shot"
+  end
+
   redirect_to users_path
+
 end
 
 private
